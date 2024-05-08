129f6b618536c362a25878f1a52bd97decbdabb9
MichalWitucki <witucki@outlook.com>
2024-05-08 07:26:56 +0200

    GameStaus entity added.

---
+++ src/main/java/ovh/witucki/signmeup/domain/game/GameStatus.java
@@ -0,0 +1,31 @@
+package ovh.witucki.signmeup.domain.gameStatus;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class GameStatus {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
---
+++ src/main/resources/db/testdata/0001_arenas_and_game_statuses.sql
@@ -0,0 +1,12 @@
+insert into arena (name, city, street, additional_info) values ('Bongor', 'Boden', '6554 Stephen Street', 'ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae mauris viverra diam vitae quam suspendisse potenti nullam');
+insert into arena (name, city, street, additional_info) values ('Naoro', 'Gabi', '14146 Hansons Alley', 'dictumst maecenas ut massa quis augue luctus tincidunt nulla mollis molestie lorem quisque');
+insert into arena (name, city, street, additional_info) values ('Buchanan', 'Jāti', '143 Marcy Road', 'at dolor quis odio consequat varius integer ac leo pellentesque ultrices');
+insert into arena (name, city, street, additional_info) values ('Jacareacanga', 'Piojó', '30466 Clarendon Parkway', 'sem sed sagittis nam congue risus semper porta volutpat quam pede lobortis ligula sit amet eleifend pede libero');
+insert into arena (name, city, street, additional_info) values ('Afton Municipal', 'Yayva', '301 Lukken Alley', 'dui nec nisi volutpat eleifend donec ut dolor morbi vel lectus in quam fringilla rhoncus mauris enim leo rhoncus sed');
+insert into arena (name, city, street, additional_info) values ('Fort Worth Meacham International', 'Granadero Baigorria', '43566 Esch Center', 'interdum in ante vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae duis');
+insert into arena (name, city, street, additional_info) values ('Coyoles', 'Beylagan', '1013 Sloan Drive', 'justo morbi ut odio cras mi pede malesuada in imperdiet et commodo vulputate justo in blandit ultrices enim lorem ipsum');
+insert into arena (name, city, street, additional_info) values ('Hatay', 'Ronggo', '69 Spaight Parkway', 'in consequat ut nulla sed accumsan felis ut at dolor quis odio consequat varius integer');
+insert into arena (name, city, street, additional_info) values ('Lorraine', 'Tanakbeak Daye', '9 Russell Junction', 'nibh in lectus pellentesque at nulla suspendisse potenti cras in purus eu magna vulputate luctus cum sociis natoque penatibus');
+insert into arena (name, city, street, additional_info) values ('Yasawa Island', 'Baroh', '619 High Crossing Circle', 'platea dictumst maecenas ut massa quis augue luctus tincidunt nulla mollis molestie lorem quisque ut erat curabitur gravida nisi');
+insert into arena (name, city, street, additional_info, picture) values ('Stadion Ruchu Radzionków', 'Bytom', 'Narutowicza 11', 'nie istnieje', 'stadion_ruchu_radzionkow020.jpg');
+
